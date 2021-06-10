6968b021d732d79b19034576ce455ad6dca76902
IMRANDIL <aliimranadil2@gmail.com>
2021-06-10 08:54:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+import people from './data.js';
+
+
+
+const container = document.querySelector('.slide-container');
+
+const prevBtn = document.querySelector('.prev-btn');
+
+const nextBtn = document.querySelector('.next-btn');
+
+
+container.innerHTML = people.map((person, slideIndex) =>{
+   let position = 'next';
+   if(slideIndex === 0){
+       position = 'active';
+   }
+
+   if(slideIndex === people.length -1 ){
+       position = 'last';
+   }
+    const {img, job, text, name} = person;
+return `  <article class="slide ${position}">
+<img
+  src="${img}"
+  class="img"
+  alt="${name}"
+/>
+<h4>${name}</h4>
+<p class="title">${job}</p>
+<p class="text">
+ ${text}
+</p>
+<div class="quote-icon">
+  <i class="fas fa-quote-right"></i>
+</div>
+</article>`
+
+}).join('');
+
+
+
+
+const startSlider = (type) =>{
+const active = document.querySelector('.active');
+const last = document.querySelector('.last');
+let next = active.nextElementSibling;
+
+if(!next){
+    next = container.firstElementChild;
+}
+
+active.classList.remove('active');
+last.classList.remove('last');
+next.classList.remove('next');
+
+
+
+if(type === 'prev'){
+    active.classList.add('next');
+    last.classList.add('active');
+   next = last.previousElementSibling;
+   if(!next){
+       next = container.lastElementChild;
+   }
+   next.classList.add('last');
+    return;
+}
+
+
+active.classList.add('last');
+next.classList.add('active');
+last.classList.add('next');
+
+}
+
+
+
+
+
+
+
+
+
+nextBtn.addEventListener('click', () =>{
+    startSlider();
+});
+
+prevBtn.addEventListener('click', () =>{
+    startSlider('prev')
+});
